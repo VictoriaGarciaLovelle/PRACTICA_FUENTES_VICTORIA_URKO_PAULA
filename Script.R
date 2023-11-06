5d9aea0d6a7b9faed236d93daa6deff6382e59bc
Urko Alli Barrena <urkobarrena03@gmail.com>
2023-11-06 10:58:58 +0100

    archivo actualizado: cambio Urko

--- Script.R
+++ Script.R
@@ -1,1 +1,12 @@
+# Importación de los datos ----
 
+## Esperanza de vida ----
+library(readr)
+EsperanzaVida <- fromJSON(file = "EsperanzaVida.json")
+head(EsperanzaVida)
+
+## Cantidad de agua consumida ----
+cantidadAgua <- fromJSON(file = "CantidadAgua.json")
+head(cantidadAgua)
+
+cantidadAgua <- spread_all(cantidadAgua)
