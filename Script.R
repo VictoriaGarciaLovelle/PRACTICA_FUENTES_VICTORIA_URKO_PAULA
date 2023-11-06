a3ed8d11f75dde1e7cd1144bf7d9c61955c4e8f6
Urko Alli Barrena <urkobarrena03@gmail.com>
2023-11-06 09:31:22 +0100

    Archivo cantidad Agua

--- Script.R
+++ Script.R
@@ -1,2 +1,15 @@
-#hola
-x<-c(1,2,3,4)
+# Importación de los datos
+
+## Calidad de las aguas de consumo humano
+library(rjson)
+
+calidadAgua <- fromJSON(file = "calidad-de-las-aguas-de-consumo-humano.json")
+head (calidadAgua)
+
+## Calidad de vida
+library(readr)
+calidad_de_vida <- read_delim("calidad_de_vida.csv", 
+                              delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+
+
