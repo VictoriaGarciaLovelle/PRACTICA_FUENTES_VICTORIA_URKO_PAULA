c2a7d54e95b93bbad1bebe24e39fe540e2ac4aa2
Paula Gregorio Losada <pgl1005@alu.ubu.es>
2023-11-06 10:50:36 +0100

    Cógigo calidad eliminado: cambio Paula

--- Script.R
+++ Script.R
@@ -1,15 +1,5 @@
 # Importación de los datos ----
 
-## Calidad de las aguas de consumo humano ----
-#library(rjson)
-
-#calidadAgua <- fromJSON(file = "calidad-de-las-aguas-de-consumo-humano.json")
-#head (calidadAgua)
-
-#library(tidyjson)
-#calidadAgua <- spread_all(calidadAgua)
-
-
 ## Esperanza de vida ----
 library(readr)
 EsperanzaVida <- fromJSON(file = "EsperanzaVida.json")
