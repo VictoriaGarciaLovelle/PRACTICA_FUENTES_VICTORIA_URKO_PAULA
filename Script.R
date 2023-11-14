aedaeda8af7e2c84591dd6e6d99a629e612cf9ff
Urko Alli Barrena <urkobarrena03@gmail.com>
2023-11-14 17:02:42 +0100

    cambios realizados en scrapping de PDF: cambio Urko

--- Script.R
+++ Script.R
@@ -24,6 +24,9 @@ cantidadAgua <- spread_all(cantidadDeAgua)
 cantidadAgua
 
 ## Calidad del agua ----
-calidadAgua <- pdftools::pdf_text("calidadDelAgua.pdf")
-
-calidadAgua
+url <- "https://www.chj.es/es-es/medioambiente/planificacionhidrologica/Documents/Plan%20de%20Recuperaci%C3%B3n%20del%20J%C3%BAcar/Cap.3_part2._Libro_blanco_del_agua.pdf"
+urls <- glue(url)
+pdf_names <- glue("report_Cap.3_part2._Libro_blanco_del_agua.pdf")
+walk2(urls, pdf_names, download.file, mode = "wb")
+raw_text <- map(pdf_names, pdf_text)
+str (raw_text)
