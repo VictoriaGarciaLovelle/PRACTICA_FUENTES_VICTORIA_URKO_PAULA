7fff5ebf6dc803d60ef4e6da2561627f2ebdfe34
VictoriaGarciaLovelle <vgl1003@alu.ubu.es>
2023-11-21 21:42:28 +0100

    probando si el select es el error

--- Script.R
+++ Script.R
@@ -116,10 +116,9 @@ sum_presupuestos <- read_delim ("C:\\Users\\G513\\Desktop\\GIS\\3\\fuentes\\pres
 view(sum_)     
 #view(sum_presupuestos)     
 
-sum_<-select(.data= sum_presupuestos,`Comunidades y Ciudades Autónomas`:`Total`) %>%
-          filter(sum_presupuestos,`Grupos de usuarios e importe` %in%
-           c("Importe total de la inversión en los servicios de suministro")) %>% 
-            na.omit(sum_presupuestos)
+sum_<-#select(.data= sum_presupuestos,`Comunidades y Ciudades Autónomas`:`Total`) %>%
+          filter(sum_presupuestos,`Grupos de usuarios e importe` %in% c("Importe total de la inversión en los servicios de suministro")) %>% 
+            drop_na()
            # summarise(.data=sum_presupuesto)
 
 
