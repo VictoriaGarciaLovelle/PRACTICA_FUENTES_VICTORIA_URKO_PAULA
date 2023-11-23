4624bf949121e56328c8de57a20cd0f0bd2af5c7
Paula Gregorio Losada <pgl1005@alu.ubu.es>
2023-11-23 11:58:07 +0100

    Eliminacion de mas codigo:Paula

--- Script.R
+++ Script.R
@@ -151,60 +151,9 @@ data
 
 
 # Ajustar los nombres de las columnas
-colnames(dataframeFin) <- c("Comunidad Autónoma", "Num de Municipios", "Zonas de baño", "Puntos de muestreo",
+colnames(data) <- c("Comunidad Autónoma", "Num de Municipios", "Zonas de baño", "Puntos de muestreo",
                                 "Aguas 2", "Aguas 1", "Aguas 0", "Aguas SCF")
-colnames(df)
-
-# Eliminar la primera fila
-df <- df[-1, ]
-
-# Convertir las columnas numéricas a tipo numérico
-cols_numericas <- colnames(df)[2:ncol(df)]
-df[cols_numericas] <- lapply(df[cols_numericas], as.numeric)
-
-# Eliminación de las tres primeras filas del df
-df <- df[,-c(1:3)]
-
-df
-
-# Crear el data frame original
-df <- data.frame(
-  Lista = I(list(
-    c("ANDALUCÍA", "58", "63", "70", "3", "36", "27", "4"),
-    c("ARAGÓN", "11", "11", "12", "3", "7", "1", "1"),
-    c("ASTURIAS", "1", "1", "1", "0", "0", "1", "0"),
-    c("CASTILLA-LA-MANCHA", "28", "39", "43", "24", "7", "12", "0"),
-    c("CASTILLA", "Y", "LEÓN", "2", "2", "2", "0", "1", "1", "0"),
-    c("CATALUÑA", "9", "10", "11", "3", "8", "0", "0"),
-    c("EXTREMADURA", "17", "17", "17", "0", "0", "0", "17"),
-    c("GALICIA", "53", "54", "68", "10", "45", "13", "0"),
-    c("MADRID", "6", "6", "7", "0", "2", "5", "0"),
-    c("MURCIA", "3", "3", "3", "0", "0", "3", "0"),
-    c("NAVARRA", "11", "11", "11", "4", "5", "2", "0"),
-    c("RIOJA", "1", "1", "1", "0", "1", "0", "0"),
-    c("VALENCIA", "2", "2", "2", "0", "1", "1", "0"),
-    c("TOTAL", "202", "220", "248", "48", "112", "66", "22")
-  ))
-)
-
-# Filtrar las filas que deseas eliminar
-regiones_a_eliminar <- c("ANDALUCÍA", "ARAGÓN", "ASTURIAS", "CASTILLA-LA-MANCHA", 
-                         "CASTILLA-Y-LEÓN", "CATALUÑA", "EXTREMADURA", "GALICIA", 
-                         "MADRID", "MURCIA", "NAVARRA", "RIOJA", "VALENCIA", "TOTAL")
-
-# Filtrar las filas que deseas eliminar
-df_filtrado <- df %>%
-  filter(!(Lista[[1]][1] %in% regiones_a_eliminar))
-
-df_filtrado
-
-# FINALIZACIÓN DE LA PRUEBA 1 --------------------------------------------------------------------------------
-
-# Proceso de prueba
-linea_deseada[4]
-
-# Los datos proporcionados
-table(linea_deseada)
+data
 
 ## -------------------- Presupuestos del agua ----------------------------
 
